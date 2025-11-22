39be082909ead38aa837556a0ff643242e0c9e1f
Ali <learn.to.code.with.ali@gmail.com>
2025-11-21 22:04:59 -0700

    updating three leds

--- kitt-leds.py
+++ kitt-leds.py
@@ -14,24 +14,23 @@ blink = 1
         
 while True:
     match(blink):
-    case 1:
-        GPIO.output(16,GPIO.HIGH)
-        GPIO.output(20,GPIO.LOW)
-        GPIO.output(21,GPIO.LOW)
-        blink=2            
-    case 2:
-        GPIO.output(16,GPIO.LOW)
-        GPIO.output(20,GPIO.HIGH)
-        GPIO.output(21,GPIO.LOW)
-        blink=3 
-    case 3:
-        GPIO.output(16,GPIO.LOW)
-        GPIO.output(20,GPIO.LOW)
-        GPIO.output(21,GPIO.HIGH)
-        blink=1 
+        case 1:
+            GPIO.output(16,GPIO.HIGH)
+            GPIO.output(20,GPIO.LOW)
+            GPIO.output(21,GPIO.LOW)
+            blink=2            
+        case 2:
+            GPIO.output(16,GPIO.LOW)
+            GPIO.output(20,GPIO.HIGH)
+            GPIO.output(21,GPIO.LOW)
+            blink=3 
+        case 3:
+            GPIO.output(16,GPIO.LOW)
+            GPIO.output(20,GPIO.LOW)
+            GPIO.output(21,GPIO.HIGH)
+            blink=1 
         
     time.sleep(1)
-    count -= 1
     
     
 print ("LED off")
